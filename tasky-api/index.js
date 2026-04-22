6aa0119b954c7f4b280c5943dddda80e0a4909b2
MatthewHoran27 <20093995@mail.wit.ie>
2026-04-22 16:46:25 +0100

    Added Auth Middleware

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import cors from 'cors';
 import usersRouter from './api/users';
+import authenticate from './authenticate';
 
 import './db';
 
@@ -28,7 +29,7 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', authenticate, tasksRouter);
 app.use('/api/users', usersRouter);
 
 app.use(errHandler);
